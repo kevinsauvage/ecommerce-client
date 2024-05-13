aede324d7e0dcbf989387216dbc9e084edb291e0
Kevin Sauvage Extia <106667569+kevinSauvage-extia@users.noreply.github.com>
2024-05-13 22:13:08 +0200

    chore: Refactor useCart to handle selected variants and quantity changes

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,33 +1,40 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
-import { Product } from "@/types";
+import { CartItem, Product, Variant } from "@/types";
 import toast from "react-hot-toast";
+import { v4 as uuidv4 } from "uuid";
 
 interface CartStore {
-  items: Array<{
-    product: Product;
-    quantity: number;
-  }>;
-  addItem: (item: Product) => void;
-  removeItem: (item: Product) => void;
+  items: Array<CartItem>;
+  addItem: (item: Product, selectedVariant?: Variant) => void;
+  removeItem: (id: string) => void;
   clearCart: () => void;
   getTotalPrice: () => number;
   getTotalItems: () => number;
-  decreaseItem: (item: Product) => void;
+  decreaseItem: (id: string) => void;
+  isQuantityIncreaseDisabledForVariant: (variant: Variant) => boolean;
+  isQuantityIncreaseDisabledForProduct: (product: Product) => boolean;
 }
 
 const useCart = create(
   persist<CartStore>(
     (set, get) => ({
       items: [],
-      addItem: (item) => {
-        const exist = get().items.find((i) => i.product?.id === item.id);
+      addItem: (item, selectedVariant) => {
+        const exist = get().items.find(
+          (i) =>
+            i.product?.id === item.id &&
+            i.selectedVariant?.id === selectedVariant?.id
+        );
 
         if (exist) {
           set((state) => ({
             items: state.items.map((i) =>
-              i.product.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
+              i.product.id === item.id &&
+              i.selectedVariant?.id === selectedVariant?.id
+                ? { ...i, quantity: i.quantity + 1, selectedVariant }
+                : i
             ),
           }));
           toast.success("Item quantity increased");
@@ -38,8 +45,10 @@ const useCart = create(
           items: [
             ...state.items,
             {
+              id: uuidv4(),
               product: item,
               quantity: 1,
+              selectedVariant,
             },
           ],
         }));
@@ -47,32 +56,55 @@ const useCart = create(
         toast.success("Item added to cart");
       },
 
-      decreaseItem: (item) => {
-        const exist = get().items.find((i) => i.product?.id === item.id);
-
-        if (exist?.quantity === 1) {
-          set((state) => ({
-            items: state.items.filter((i) => i.product.id !== item.id),
-          }));
-        } else {
-          set((state) => ({
-            items: state.items.map((i) =>
-              i.product.id === item.id ? { ...i, quantity: i.quantity - 1 } : i
-            ),
-          }));
-        }
+      decreaseItem: (id) => {
+        set((state) => ({
+          items: state.items.map((i) =>
+            i.id === id ? { ...i, quantity: i.quantity - 1 } : i
+          ),
+        }));
         toast.success("quantity correctly decreased");
       },
-      removeItem: (item) => {
+      removeItem: (id) => {
         set((state) => ({
-          items: state.items.filter((i) => i.product.id !== item.id),
+          items: state.items.filter((i) => i.id !== id),
         }));
         toast.success("Item removed from cart");
       },
       clearCart: () => set({ items: [] }),
       getTotalPrice: () =>
-        get().items.reduce((acc, item) => acc + Number(item.product.price), 0),
-      getTotalItems: () => get().items.length,
+        get().items.reduce(
+          (acc, item) => acc + Number(item.product.price) * item.quantity,
+          0
+        ),
+      getTotalItems: () =>
+        get().items.reduce((acc, item) => acc + item.quantity, 0),
+
+      isQuantityIncreaseDisabledForVariant: (variant) => {
+        if (variant?.stock <= 0) return true;
+
+        const cartItem = get().items.find(
+          (item) => item.selectedVariant?.id === variant.id
+        );
+
+        return (
+          (cartItem &&
+            cartItem.quantity >=
+              Number(cartItem?.selectedVariant?.stock || 0) - 1) ||
+          false
+        );
+      },
+
+      isQuantityIncreaseDisabledForProduct: (product) => {
+        if (product.stock <= 0) return true;
+
+        const cartItem = get().items.find(
+          (item) => item.product.id === product.id
+        );
+
+        return (
+          (cartItem && cartItem.quantity >= cartItem.product.stock) || false
+        );
+      },
     }),
     {
       name: "cart-storage",
