7d3e83b0838b219f488d228f8eccdfdf5d5b0d55
Kevin Sauvage Extia <106667569+kevinSauvage-extia@users.noreply.github.com>
2024-05-13 22:12:44 +0200

    chore: Refactor ProductInfo component to use ProductInfoActions

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -1,25 +1,13 @@
-"use client";
-
 import { Product } from "@/types";
-import { Button } from "./ui/button";
 import Currency from "./Currency";
 import { Separator } from "./ui/separator";
-import useCart from "@/hooks/useCart";
-import { useMemo } from "react";
+import ProductInfoActions from "./ProductInfoActions";
 
 export default function ProductInfo({ product }: { product: Product }) {
-  const cartStore = useCart();
-
   const { name, description, price, variants } = product || {};
 
-  const colors = useMemo(
-    () => [...new Set(variants.map((v) => v.color.value))],
-    [variants]
-  );
-  const sizes = useMemo(
-    () => [...new Set(variants.map((v) => v.size.name))],
-    [variants]
-  );
+  const colors = [...new Set(variants?.map((v) => v.color.value))];
+  const sizes = [...new Set(variants?.map((v) => v.size.name))];
 
   return (
     <div className="grid gap-4 md:gap-10 items-start">
@@ -32,38 +20,8 @@ export default function ProductInfo({ product }: { product: Product }) {
           <Currency price={price} />
         </div>
         <Separator />
-        <div className="grid gap-4">
-          <div className="flex items-center gap-2">
-            <span className="text-sm  font-semibold">Color: </span>
-            {colors?.map((color) => (
-              <div
-                key={color}
-                style={{
-                  backgroundColor: color,
-                }}
-                className="w-6 h-6 rounded-full border border-gray-200 dark:border-gray-800"
-              />
-            ))}
-          </div>
-          <div className="flex items-center gap-2">
-            <span className="text-sm font-semibold">Size:</span>
-            {sizes?.map((size) => (
-              <div key={size} className="text-sm">
-                {size}
-              </div>
-            ))}
-          </div>
-        </div>
       </div>
-      <Button
-        size="lg"
-        variant="default"
-        onClick={() => {
-          cartStore.addItem(product);
-        }}
-      >
-        Add to cart
-      </Button>
+      <ProductInfoActions product={product} colors={colors} sizes={sizes} />
     </div>
   );
 }
